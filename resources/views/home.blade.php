b45e8e7e60a6d7f74e132a69c54c643a2958d19f
chiragpandey15 <chiragpandey15@gmail.com>
2021-03-24 04:01:15 +0530

    all done right?

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,30 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                        {{ Name: $user['name'] }} <br>
+                        {{ Email: $user['email'] }} <br>
+                        {{ Password: $user['password'] }} <br>
 
-                    {{ Name: $user['name'] }} <br>
-                    {{ Email: $user['email'] }} <br>
-                    {{ Password: $user['password'] }} <br>
-
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+</body>
+</html>
